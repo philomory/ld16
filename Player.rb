4d0ec0e8137d81f8ed3b5761ff80f4431f7c6d62
philomory <philomory@localhost>
2010-02-23 09:31:03 -1000

    This file should have also been part of the Upgrades commit

--- Player.rb
+++ Player.rb
@@ -1,16 +1,17 @@
 require 'GridSquare'
+require 'Upgradable'
 
 module LD16
   class Player
-    attr_accessor :fuel, :score, :x, :y
-    attr_reader :max_fuel, :upgrades, :sight
+    include Upgradable
+    
+    attr_accessor :fuel, :score, :x, :y, :max_fuel, :sight
     def initialize(x,y,game)
       @x,@y,@game = x,y,game
-      @max_fuel   =      1000
+      @max_fuel   =      5000
       @fuel       = @max_fuel
       @score      =         0
       @sight      =         4
-      @upgrades   =        []
       self.update_sight
     end
     
@@ -20,18 +21,20 @@ module LD16
         @game.pass_edge(dir)
       else
         cost = destination.contents.cost
-        if @fuel >= cost
-          @fuel -= cost
-          @x,@y = *destination
-          self.update_sight
-        else
-          #flash fuel later
+        if self.can_traverse(destination.contents)
+          if @fuel >= cost
+            @fuel -= cost
+            @x,@y = *destination
+            self.update_sight
+          else
+            #flash fuel later
+          end
         end
       end
     end
     
     def update_sight
-      @game.region.seen.around(@x,@y,@sight).each do |sq|
+      @game.region.seen.around(@x,@y,self.sight).each do |sq|
         unless @game.region.seen[*sq]
           @score += @game.region.terrain[*sq].value
           @game.region.seen[*sq] = true
@@ -41,11 +44,22 @@ module LD16
     
     def wait
       @fuel += 30
-      @fuel = 1000 if @fuel > 1000
+      @fuel = [@fuel, self.max_fuel].min
     end
     
     def draw
       @game.draw_grid_square(@x,@y,0xFFFF00FF,1,1)
     end
+    
+    def can_traverse(terrain)
+      case terrain
+      when Terrain::Water then false
+      when Terrain::Mountain then false
+      else true
+      end
+    end
+    
+
+    
   end
 end
