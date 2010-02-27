81f674b9527210504d3adc4b51e1da3e4aa6d4b7
philomory <philomory@localhost>
2010-02-26 22:25:34 -1000

    Building up the dungeon.

--- Dungeon.rb
+++ Dungeon.rb
@@ -1,10 +1,24 @@
+require 'Grid'
+require 'PRNG'
+
 module LD16
   class Dungeon
-    include Screen
+    NUMBER_OF_ROOMS = 4
     def initialize(seed)
-      init_screen
       @seed = seed
       @prng = PRNG.new(seed)
+      @map = Grid.new(Sizes::RegionWidth,Sizes::RegionHeight) {0}
+      cells_for_rooms = []
+      supergrid = Grid.new(3,3) {false}
+      cells = supergrid.grid_squares
+      NUMBER_OF_ROOMS.times do
+        cell = cells[@prng.next(cells.size)]
+        cells.delete(cell)
+        cells_for_rooms << cell
+      end
+      
+      p cells_for_rooms
+      
     end
     
     
