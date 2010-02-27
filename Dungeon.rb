bda89e17ae5deb2a494efae898f857a2fd6483ac
philomory <philomory@localhost>
2010-02-26 23:13:02 -1000

    More dungeon generation.

--- Dungeon.rb
+++ Dungeon.rb
@@ -1,23 +1,41 @@
 require 'Grid'
 require 'PRNG'
+require 'helper'
 
 module LD16
+  # Dungeon generation following algorithm here:
+  # http://roguebasin.roguelikedevelopment.org/index.php?title=Grid_Based_Dungeon_Generator
   class Dungeon
     NUMBER_OF_ROOMS = 4
-    def initialize(seed)
+    def initialize(seed,cell_width,cell_height,cells_wide,cells_high)
       @seed = seed
+      @width, @height = cell_width * cells_wide, cell_height * cells_high
+      
       @prng = PRNG.new(seed)
-      @map = Grid.new(Sizes::RegionWidth,Sizes::RegionHeight) {0}
+      @map = Grid.fill(@width,@height) {0}
       cells_for_rooms = []
-      supergrid = Grid.new(3,3) {false}
+      supergrid = Grid.new(cells_wide,cells_high)
       cells = supergrid.grid_squares
       NUMBER_OF_ROOMS.times do
         cell = cells[@prng.next(cells.size)]
         cells.delete(cell)
         cells_for_rooms << cell
       end
+      cells_for_rooms.each do |cell|
+        cell_x = cell.x * cell_width + 1
+        cell_y = cell.y * cell_height + 1
+        w = cell_width - 2
+        h = cell_height - 2
+        @map.map_with_coords! do |val,x,y|
+          if x.between?(cell_x,cell_x+w) && y.between?(cell_y,cell_y+h)
+            1
+          else
+            val  
+          end
+        end
+      end
       
-      p cells_for_rooms
+      p @map
       
     end
     
